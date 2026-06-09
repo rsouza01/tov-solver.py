b03c5ab0f23a4d8c18770326d85b4ff0f6a02918
Rodrigo de Souza <rsouza01@gmail.com>
2026-06-09 20:05:08 +0200

    Action fixed

--- src/tov_solver/domains/tov/cli.py
+++ src/tov_solver/domains/tov/cli.py
@@ -17,7 +17,7 @@ def register_commands(subparsers: argparse._SubParsersAction):
 		title="commands", dest="command", required=True
 	)
 
-	# Command: tov-solver.py tov integrate --pc 1e35 --eos SLy
+	# Command: tov_solver.py tov integrate --pc 1e35 --eos SLy
 	int_parser = tov_subparsers.add_parser("integrate", help="Integrate hydrostatic equilibrium equations")
 	int_parser.add_argument("--pc", type=float, required=True, help="Central pressure")
 	int_parser.add_argument("--eos", type=str, required=True, help="Identifier of the EOS to use")
