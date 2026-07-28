cd2a49378550c3d398b3a649f33ac39a40203af1
Rodrigo de Souza <rsouza01@gmail.com>
2026-07-28 23:05:07 +0200

    Small SonarQube comment

--- src/tov_solver/main.py
+++ src/tov_solver/main.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python3
 
-
 def main() -> None:
-    # parser = argparse.ArgumentParser(
-    #     prog="tov-solver",
-    #     description="CLI suite for stellar evolution and compact object simulations."
-    # )
     pass
-
 
 if __name__ == "__main__":
     main()
